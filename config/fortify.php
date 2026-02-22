696f1272339fc2cdcaf77b7855f7c2f79db074a6
Ray Georpe <raymarlou06@gmail.com>
2026-02-22 15:51:43 +0800

    final ywa

--- config/fortify.php
+++ config/fortify.php
@@ -158,7 +158,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(), // Disabled: no new account registration
         Features::resetPasswords(),
         Features::emailVerification(),
         Features::twoFactorAuthentication([
